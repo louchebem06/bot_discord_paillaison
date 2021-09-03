8046f28b7f908e029c17c6000df3d55aaa4aae44
Ledda Bryan <louchebem06@gmail.com>
2021-09-03 16:59:47 +0200

    Big Update

--- commands/log.js
+++ commands/log.js
@@ -8,36 +8,71 @@ const Discord 		= require("discord.js");
 
 momentFormat(moment);
 
-function data_req(token, message, user)
+function logtime(data)
 {
-	const options = {
-		host : "api.intra.42.fr",
-		path: `/v2/users/${user}/locations?per_page=100`,
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	};
-	var req = https.request(options, function(res)
+	prev_month_i = moment().utc().subtract(1,"month").month();
+	final = moment().utc();
+	this_final = moment().utc();
+
+	for (var i = 0; data[i]; i++)
 	{
-		res.setEncoding('utf8');
-		var data = ''
-		res.on('data', function (chunk)
+		this_end = moment(data[i].end_at);
+		this_begin = moment(data[i].begin_at);
+		if (prev_month_i == moment(data[i].begin_at).month())
 		{
-			data += chunk;
-		});
-		res.on('end', function ()
+			if (data[i].end_at === null)
+				final.add(moment().utc().subtract(this_begin));
+			else if (this_end.month() === moment().utc().month())
+			{
+				final.add(this_end.clone().hour(0).minute(0).subtract(this_begin));
+				this_final.add(this_end.subtract(this_end.clone().hour(0).minute(0)))
+			}
+			else
+				final.add(this_end.subtract(this_begin));
+		}
+		else if (moment().utc().month() == moment(data[i].begin_at).month())
 		{
-			const obj = JSON.parse(data);
-			logtime(obj, message, token);
-		});
-	});
-	req.on('error', function(e) {
-	  message.channel.send("❌ Error : user is not valid");
-	});
-	req.end();
+			if (data[i].end_at === null)
+				this_final.add(moment().utc().subtract(this_begin));
+			else
+				this_final.add(this_end.subtract(this_begin));
+		}
+	}
+
+	var last_month = moment.duration(final.subtract(moment())).format("h:mm");
+	var current_month = moment.duration(this_final.subtract(moment())).format("h:mm");
+
+	var tab = {
+		'last_month' : last_month,
+		'current_month' : current_month
+	}
+	return (tab);
 }
 
-function sendEmbed(url_coa, color_coa, last_month, current_month, message, login, full_name, url_image)
+function api42(token, url)
+{
+	return new Promise((resolve, reject) => {
+		const options = {
+			host : "api.intra.42.fr",
+			path: url,
+			headers: {Authorization: `Bearer ${token}`}
+		};
+		var req = https.request(options, (res) => {
+			var data = '';
+			res.setEncoding('utf8');
+			res.on('data',(chunk) => {data += chunk});
+			res.on('end', () =>{
+				try{
+	            	const obj = JSON.parse(data);
+	            	resolve(obj);
+	          	}catch (e) {reject(null)}
+			}).on('error', (e) => {reject(null)});
+		});
+		req.end();
+	});
+}
+
+function sendEmbed(token, message, login)
 {
 	/*
 		url_coa
@@ -45,132 +80,53 @@ function sendEmbed(url_coa, color_coa, last_month, current_month, message, login
 		svg is not support by discord
 		convert svg to png for view in footer
 	*/
-	const url_intra = `https://profile.intra.42.fr/users/${login}`;
-	const embed = new Discord.MessageEmbed()
-		.setColor(color_coa)
-    	.setTitle(`Logtime ${login}`)
-    	.setAuthor(full_name, url_image, url_intra)
-    	.setTimestamp(new Date())
-    	.setFooter(`Logtime ${login}`, url_coa);
+	const err_user	= "❌ Error : User not found";
+	const err_api	= "❌ Error : Too many request 42 API";
 
-    embed.addField("Last month",
-    	last_month
-    	,false);
-    embed.addField("Current month",
-    	current_month
-    	,false);
-
-    message.channel.send({embed: embed});
-}
-
-function coaInfo(token, last_month, current_month, id_user, message, login, full_name, url_image)
-{
-	const options = {
-		host : "api.intra.42.fr",
-		path: `/v2/users/${id_user}/coalitions`,
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	};
-	var req = https.request(options, function(res)
+	var req_locations = api42(token, `/v2/users/${login}/locations?per_page=100`);
+	req_locations.then((locations)=>
 	{
-		res.setEncoding('utf8');
-		var data = ''
-		res.on('data', function (chunk)
+		if (locations[0])
 		{
-			data += chunk;
-		});
-		res.on('end', function ()
-		{
-			const coa_info = JSON.parse(data);
-
-			const url_coa 	= coa_info[0].image_url;
-			const color_coa 	= coa_info[0].color;
-
-			sendEmbed(url_coa, color_coa, last_month, current_month, message, login, full_name, url_image);
-		});
-	});
-	req.on('error', function(e) {
-	  message.channel.send("❌ Error : Message create");
-	});
-	req.end();
-}
-
-function userInfo(token, last_month, current_month, id_user, message)
-{
-	setTimeout(() => {console.log("Pause 2 sec pour API 42");}, 2000);
-	const options = {
-		host : "api.intra.42.fr",
-		path: `/v2/users/${id_user}`,
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	};
-	var req = https.request(options, function(res)
-	{
-		res.setEncoding('utf8');
-		var data = ''
-		res.on('data', function (chunk)
-		{
-			data += chunk;
-		});
-		res.on('end', function ()
-		{
-			const user_info = JSON.parse(data);
-
-			const login 	= user_info.login;
-			const full_name = user_info.usual_full_name;
-			const url_image = user_info.image_url;
-
-			coaInfo(token, last_month, current_month, id_user, message, login, full_name, url_image);
-		});
-	});
-	req.on('error', function(e) {
-	  message.channel.send("❌ Error : Message create");
-	});
-	req.end();
-}
-
-function logtime(data, message, token)
-{
-	if (data[0])
-	{
-		prev_month_i = moment().utc().subtract(1,"month").month();
-		final = moment().utc();
-		this_final = moment().utc();
-		for (var i = 0; data[i]; i++)
-		{
-			this_end = moment(data[i].end_at);
-			this_begin = moment(data[i].begin_at);
-			if (prev_month_i == moment(data[i].begin_at).month())
+			setTimeout(() => {console.log("Pause 1 sec pour API 42");}, 1500);
+			var req_coa_info = api42(token, `/v2/users/${locations[0].user.id}/coalitions`);
+			req_coa_info.then((coa_info)=>
 			{
-				if (data[i].end_at === null)
-					final.add(moment().utc().subtract(this_begin));
-				else if (this_end.month() === moment().utc().month())
+				setTimeout(() => {console.log("Pause 1 sec pour API 42");}, 1500);
+				var req_user_info = api42(token, `/v2/users/${locations[0].user.id}`);
+				req_user_info.then((user_info)=>
 				{
-					final.add(this_end.clone().hour(0).minute(0).subtract(this_begin));
-					this_final.add(this_end.subtract(this_end.clone().hour(0).minute(0)))
-				}
-				else
-					final.add(this_end.subtract(this_begin));
-			}
-			else if (moment().utc().month() == moment(data[i].begin_at).month())
-			{
-				if (data[i].end_at === null)
-					this_final.add(moment().utc().subtract(this_begin));
-				else
-					this_final.add(this_end.subtract(this_begin));
-			}
+					var hours = logtime(locations);
+					// const url_coa 	= coa_info[0].image_url;
+					const color_coa 	= coa_info[0].color;
+					const full_name 	= user_info.usual_full_name;
+					const url_image 	= user_info.image_url;
+					const url_intra 	= `https://profile.intra.42.fr/users/${login}`;
+
+					const embed = new Discord.MessageEmbed()
+						.setColor(color_coa)
+						.setTitle(`Logtime ${login}`)
+						.setAuthor(full_name, url_image, url_intra)
+						.setTimestamp(new Date())
+						//.setFooter(`Logtime ${login}`);
+
+					embed.addField("Last month",
+						hours.last_month
+						,false);
+					embed.addField("Current month",
+						hours.current_month
+						,false);
+
+					message.channel.send({embed: embed});
+				});
+				req_user_info.catch((error)=>{message.channel.send(err_api)});
+			});
+			req_coa_info.catch((error)=>{message.channel.send(err_api)});
 		}
-		var last_month = moment.duration(final.subtract(moment())).format("h:mm");
-		var current_month = moment.duration(this_final.subtract(moment())).format("h:mm");
-
-		const id_user = data[0].user.id;
-
-		userInfo(token, last_month, current_month, id_user, message)
-	}
-	else
-		message.channel.send("❌ Error : user is not valid");
+		else
+			message.channel.send(err_user);
+	});
+	req_locations.catch((error)=>{message.channel.send(err_api)});
 }
 
 module.exports = class log extends Command
@@ -197,7 +153,7 @@ module.exports = class log extends Command
 			    });
 			    res.on('end', function ()
 			    {
-			       	data_req(data, message, av[2]);
+			       	sendEmbed(data, message, av[2]);
 			    });
 			});
 			request.on('error', function (e)
