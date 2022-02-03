acb595a30989fb8cb5739e94e91384f75ee5f05e
Ledda Bryan <louchebem06@gmail.com>
2022-02-03 23:42:01 +0100

    update

--- commands/log.js
+++ commands/log.js
@@ -5,7 +5,8 @@ const https 		= require('https');
 const moment		= require('moment');
 const momentFormat	= require("moment-duration-format");
 const Discord 		= require("discord.js");
-const convertapi 	= require('convertapi')(config.CONVERTAPI);	
+const convertapi 	= require('convertapi')(config.CONVERTAPI);
+const Axios 		= require("axios");
 
 momentFormat(moment);
 
@@ -79,6 +80,8 @@ function sendEmbed(token, message, login)
 	const err_api			= "❌ Error : Too many request 42 API";
 	const err_apiconvert	= "❌ Error : The quota of 1500 on convert api has been exceeded";
 
+	token = JSON.parse(token)['access_token'];
+	message.channel.send("I am search");
 	var req_locations = api42(token, `/v2/users/${login}/locations?per_page=100`);
 	req_locations.then((locations)=>
 	{
@@ -88,6 +91,7 @@ function sendEmbed(token, message, login)
 			var req_coa_info = api42(token, `/v2/users/${locations[0].user.id}/coalitions`);
 			req_coa_info.then((coa_info)=>
 			{
+				message.channel.send("Data ok");
 				setTimeout(() => {console.log("Pause 1 sec pour API 42");}, 1500);
 				var req_user_info = api42(token, `/v2/users/${locations[0].user.id}`);
 				req_user_info.then((user_info)=>
@@ -99,6 +103,7 @@ function sendEmbed(token, message, login)
 					const url_image 	= user_info.image_url;
 					const url_intra 	= `https://profile.intra.42.fr/users/${login}`;
 
+					message.channel.send("I take care of your photo");
 					convertapi.convert('png', {
 					    File: url_coa,
 					    FileName: login
@@ -125,14 +130,17 @@ function sendEmbed(token, message, login)
 					}).catch((error)=>{message.channel.send(err_apiconvert)});
 				});
 				req_user_info.catch((error)=>{message.channel.send(err_api)});
-			});
-			req_coa_info.catch((error)=>{message.channel.send(err_api)});
+			})
+			.catch((error)=>{message.channel.send(err_api)});
 		}
 		else
 			message.channel.send(err_user);
-	});
-	req_locations.catch((error)=>{message.channel.send(err_api)});
+	})
+	.catch((error)=>{message.channel.send(err_api)});
 }
+
+const UID = config.UID_42;
+const SECRET = config.SECRET_42;
 
 module.exports = class log extends Command
 {
@@ -147,26 +155,40 @@ module.exports = class log extends Command
 		const av = msg.split(" ");
 		if (av[2] && !av[3])
 		{
-			const baddel_token = config.BADDEL_TOKEN;
-			const REQ = 'http://baddel.fr/discord/bot.php?json&token='+config.BADDEL_TOKEN;
-			var request = http.request(REQ, function (res)
-			{
-			    var data = '';
-			    res.on('data', function (chunk)
-			    {
-			        data += chunk;
-			    });
-			    res.on('end', function ()
-			    {
-			       	sendEmbed(data, message, av[2]);
-			    });
+			const url = "api.intra.42.fr";
+			const querystring = require('querystring');
+			const https = require('https')
+			const postData = querystring.stringify({
+				'grant_type': 'client_credentials',
+				'client_id': `${UID}`,
+				'client_secret': `${SECRET}`
 			});
-			request.on('error', function (e)
-			{
-			    message.channel.send("❌ Error : baddel in PLS");
-			    return ;
-			});
-			request.end();
+		
+			const options = {
+				"hostname": url,
+				"method": "POST",
+				"path" : "/oauth/token",
+				"port" : 443,
+				"encoding": "utf8",
+				"followRedirect": true,
+				"headers": {
+					"Authorization": `Basic <base64 encoded client_id:client_secret>`,
+					"Content-Type": 'application/x-www-form-urlencoded',
+					"Content-Length": Buffer.byteLength(postData),
+				},
+				'muteHttpExceptions': true
+				}
+		
+			const req = https.request(options, res => {
+				res.on('data', d => {
+					sendEmbed(""+d, message, av[2]);
+				})
+			})
+			req.on('error', error => {
+				console.error(error)
+			})
+			req.write(postData);
+			req.end()
 		}
 		else
 			message.channel.send("❌ Error : self-destruct");
